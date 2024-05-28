5b5cd6b1ce35668baddee828f07c11d81b7d8c92
Neil Batchelor <nb@encode.host>
2024-05-28 18:23:18 +0100

    Added Matomo & Piwik files. Reconfigured send-stats

--- send-stats.php
+++ send-stats.php
@@ -1,6 +1,8 @@
 <?php
 
-add_action( 'wp_head' , 'lap_matomo_http_api_head' );
+add_action( 'wp_body_open' , 'lap_matomo_http_api_head' );
+
+/*
 function lap_matomo_http_api_head() {
   $options = get_option( 'lap_matomo_http_api_options' );
   if ($options['url'] !== false) {
@@ -9,11 +11,36 @@ function lap_matomo_http_api_head() {
     $url = ($options['url']);
     $idsite = ($options['idsite']);
     $tracking_args = 'rec=1';
+    $queuedtracking = 0;
     $wp_url = esc_url('https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
     $page_title = get_the_title();
 
-    $full_url = $url. 'matomo.php?idsite='. $idsite . '&' . $tracking_args . '&url='. $wp_url ; 
-    //var_dump($full_url);
-    wp_remote_post( $full_url);
+    $full_url = $url. 'matomo.php?idsite='. $idsite . '&amp;action_name=doTrackPageView&amp;' . $tracking_args . '&amp;url='. urlencode($wp_url) .'&queuedtracking='. $queuedtracking. '&amp;title='. urlencode($page_title); 
+    // var_dump($full_url);
+    echo '<img src="'. $full_url . '" alt="">';
   }
-} 
+} */
+
+function lap_matomo_http_api_head() {
+  include_once plugin_dir_path( __FILE__ ) .'MatomoTracker.php';
+
+  $options = get_option( 'lap_matomo_http_api_options' );
+  if ($options['url'] !== false) {
+    global $wp;
+
+    $url = ($options['url']);
+    $idsite = ($options['idsite']);
+    $tracking_args = 'rec=1';
+    $queuedtracking = 0;
+    $wp_url = esc_url('https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+    $page_title = esc_html(get_the_title());
+    $user_info = get_userdata(get_current_user_id());
+    $display_name = $user_info->display_name;
+    echo $display_name;
+
+    MatomoTracker::$URL = $url;
+    $matomoTracker = new MatomoTracker( $idSite = $idsite );
+    $matomoTracker->doTrackPageView( $page_title );
+    $matomoTracker->setUserId($display_name);
+  }
+}
